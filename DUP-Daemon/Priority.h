497acc11cd6c32f4ef87255e05b91b52b613c307
seg--fault <rbkillea@wpi.edu>
2013-10-10 22:37:57 -0400

    architecture for the priority queue pt. 1

--- DUP-Daemon/Priority.h
+++ DUP-Daemon/Priority.h
@@ -1,14 +1,30 @@
 #if !defined(PRIORITY_H)
 #define PRIORITY_H
-#include "Logger.h"
+#include "Logic.h"
+
+#define DUP_LOCALSTORESIZE 256
 
 class PriorityQueue
 {
+};
 
+class HashEntry
+{
+public:
+	HashEntry(char *toSerialize, size_t length);
+
+private:
+	void *continuation;
+	char localstore[256];
 };
 
 class HashTable
 {
+public:
+	HashTable(size_t length);
+
+private:
+	HashEntry table[];
 };
 
 #endif
